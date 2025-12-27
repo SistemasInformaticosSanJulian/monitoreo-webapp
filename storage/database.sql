b3b0d12124b1aa594e837907e443389e7fbdb4ea
Juan Vladimir <juanvladimir13@gmail.com>
2025-12-26 22:42:58 -0400

    feat(db): change database

--- storage/database.sql
+++ storage/database.sql
@@ -1,9 +1,9 @@
 CREATE TABLE oficina
 (
     id          integer primary key autoincrement,
-    nombre      varchar(50)  not null,
-    direccion varchar(255) not null,
-    celular varchar(15) not null,
+    nombre      varchar(35)  not null,
+    direccion   varchar(150) not null,
+    celular     varchar(15)  not null,
     soft_delete boolean      not null default 0,
     unique (nombre)
 );
@@ -11,10 +11,10 @@ CREATE TABLE oficina
 CREATE TABLE operador
 (
     id          integer primary key autoincrement,
-    nombre      varchar(50)  not null,
+    nombre      varchar(75)  not null,
     contrasenia varchar(255) not null,
-    rol        varchar(20)  not null default 'operador',
-    oficina_id int not null,
+    rol         varchar(15)  not null default 'operador',
+    oficina_id  integer      not null,
     soft_delete boolean      not null default 0,
     created_at  timestamp             default CURRENT_TIMESTAMP,
     updated_at  timestamp,
@@ -25,7 +25,7 @@ CREATE TABLE operador
 CREATE TABLE usuario
 (
     id          integer primary key autoincrement,
-    nombre      varchar(50)  not null,
+    nombre      varchar(75)  not null,
     contrasenia varchar(255) not null,
     soft_delete boolean      not null default 0,
     created_at  timestamp             default CURRENT_TIMESTAMP,
@@ -35,26 +35,29 @@ CREATE TABLE usuario
 
 create table cliente
 (
-    id                 integer primary key autoincrement,
-    nombres            varchar(50) not null,
-    apellidos          varchar(50) not null,
-    direccion          varchar(50),
-    correo_electronico varchar(70) not null,
-    celular            varchar(15) not null,
-    usuario_id         int         not null,
-    foreign key (usuario_id) references usuario (id)
+    id          integer primary key autoincrement,
+    nombres     varchar(50) not null,
+    apellidos   varchar(50) not null,
+    direccion   varchar(50),
+    celular     varchar(15) not null,
+    usuario_id  integer     not null,
+    soft_delete boolean     not null default 0,
+    foreign key (usuario_id) references usuario (id) on delete cascade on update cascade
 );
 
 create table conductor
 (
     id               integer primary key autoincrement,
-    nombres          varchar(35) not null,
-    apellidos          varchar(35) not null,
-    carnet           varchar(20) not null,
+    nombres          varchar(75) not null,
+    apellidos        varchar(75) not null,
+    carnet           varchar(15) not null,
     fecha_nacimiento date        not null,
     celular          varchar(15) not null,
-    correo           varchar(40),
-    direccion        varchar(75),
+    correo           varchar(75),
+    direccion        varchar(150),
+    soft_delete      boolean     not null default 0,
+    created_at       timestamp            default CURRENT_TIMESTAMP,
+    updated_at       timestamp,
     unique (carnet)
 );
 
@@ -63,28 +66,35 @@ create table movil
     id                integer primary key autoincrement,
     interno           varchar(3)  not null,
     placa             varchar(10) not null,
-    cantidad_asientos int         not null,
+    cantidad_asientos integer     not null,
     conductor_id      integer     not null,
-    unique (interno),
+    soft_delete       boolean     not null default 0,
+    created_at        timestamp            default CURRENT_TIMESTAMP,
+    updated_at        timestamp,
+    unique (placa),
     foreign key (conductor_id) references conductor (id)
 );
 
 create table viaje
 (
     id                integer primary key autoincrement,
-    codigo            varchar(25) not null,
-    origen            varchar(50) not null,
-    destino           varchar(50) not null,
-    fechahora_salida  datetime    not null,
+    codigo            varchar(25)  not null,
+    origen            varchar(50)  not null,
+    destino           varchar(50)  not null,
+    fechahora_salida  datetime     not null,
     fechahora_llegada datetime,
-    encomienda        varchar(35) not null,
-    direccion_entrega  varchar(50) not null,
-    emisor            varchar(50) not null,
-    receptor          varchar(50) not null,
-    estado            varchar(15) not null,
-    movil_id          integer     not null,
-    soft_delete       boolean     not null default 0,
-    foreign key (movil_id) references movil (id)
+    encomienda        varchar(250) not null,
+    direccion_entrega varchar(150) not null,
+    emisor            varchar(75)  not null,
+    receptor          varchar(75)  not null,
+    estado            varchar(15)  not null,
+    movil_id          integer      not null,
+    operador_id       integer      not null,
+    oficina_id        integer      not null,
+    soft_delete       boolean      not null default 0,
+    foreign key (movil_id) references movil (id),
+    foreign key (operador_id) references operador (id),
+    foreign key (oficina_id) references oficina (id)
 );
 
 create table monitoreo
@@ -93,7 +103,7 @@ create table monitoreo
     latitud    real        not null,
     longitud   real        not null,
     codigo     varchar(25) not null,
-    viaje_id   int         not null,
+    viaje_id   integer     not null,
     updated_at timestamp default CURRENT_TIMESTAMP,
     unique (viaje_id)
 );
